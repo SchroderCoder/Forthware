0c548259815163bf6bcfd977d4136413b7266a5a
unknown <a01707503@itesm.mx>
2022-09-18 19:03:48 -0500

    agregar carpetas css,js y mejorar funcionalidad de crear tarea

--- controllers/tareas.controller.js
+++ controllers/tareas.controller.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const session = require('express-session');
-const Proyecto = require('../models/tarea.model');
+const Proyecto = require('../models/proyecto.model');
 const Tarea = require('../models/tarea.model');
 const Usuario = require('../models/user.model');
 
@@ -18,18 +18,32 @@ exports.getCrearTareas = (request, response, next) => {
             for(let empleado of rows) {
                 request.session.empleados.push(empleado.nombre);
             }
-            response.render(path.join('..',"views", "CrearTarea.ejs"), {
-                privilegios: request.session.privilegios,
-                empleados: request.session.empleados,
-            });
+            Proyecto.fetchAll()
+                .then(([consulta, fielData]) => {
+                    request.session.proyectos= [];
+                    for(let proyecto of consulta) {
+                        request.session.proyectos.push(proyecto.nombre);
+                    }
+                    
+                    response.render(path.join('..',"views", "CrearTarea.ejs"), {
+                        privilegios: request.session.privilegios,
+                        empleados: request.session.empleados,
+                        proyectos: request.session.proyectos,
+                    });
+                })
+
+                .catch(err => {
+                    response.render('error.ejs');
+                }); 
         })
         .catch(err => {
-            console.log('no salio');
             response.render('error.ejs');
         }); 
 }
 
 exports.postCrearTareas = (request, response, next) => {
+    console.log(request.body.empleados);
+
     const tarea = new Tarea("w",0.5,2,"2022/08/09");
         
         tarea.save()
