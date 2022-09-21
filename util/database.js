25236bbfbfd7b97e5e31026a1006c98169ae8147
DonCake <71788652+DonCake@users.noreply.github.com>
2022-09-20 19:53:53 -0500

    Main.ejs con head,navbar,sidebar .ejs y ajustes a estos mismos

--- util/database.js
+++ util/database.js
@@ -3,8 +3,9 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'natdev',
-    password: ''
+    database: 'Natdev',
+    password: 'root',
+    port: 8889,
 });
 
 module.exports = pool.promise();
