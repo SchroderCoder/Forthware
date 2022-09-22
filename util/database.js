41ab4cff4479966cade18237e5e0a525022dd69b
DonCake <71788652+DonCake@users.noreply.github.com>
2022-09-22 18:39:31 -0500

    Tarjetade poryectos con scroll y editar etiqueta pop up

--- util/database.js
+++ util/database.js
@@ -3,8 +3,9 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'natdev 2.0',
-    password: '',
+    database: 'Natdev',
+    password: 'root',
+    port: 8889,
 });
 
 module.exports = pool.promise();
