7412b2aa569dcb2757f77230f7532d5d84106f0a
unknown <a01707503@itesm.mx>
2022-09-23 00:36:25 -0500

    arreglar moment y pruebas finales

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'natdev 2.0',
+    database: 'natdev',
     password: '',
 });
 
