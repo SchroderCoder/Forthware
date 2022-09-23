4ebfb147986bab79791412d876fda0de20bab301
DonCake <71788652+DonCake@users.noreply.github.com>
2022-09-22 21:47:53 -0500

    Proyectos tarjetas casi completas

--- util/database.js
+++ util/database.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'natdev',
-    password: '',
+    password: 'root',
+    port: 8889,
 });
 
 module.exports = pool.promise();
