2aec97dad4a58027afaed9177e78e74b716c0c7f
unknown <a01707503@itesm.mx>
2022-09-28 19:18:26 -0500

    proteger rutas con Auth0

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,20 +4,14 @@ const { requiresAuth } = require('express-openid-connect');
 const userController = require('../controllers/user.controller');
 
 
-router.get('/new',  userController.getNew);
+router.get('/new',  requiresAuth(),userController.getNew);
 
-router.post('/new',  userController.postNew);
+router.post('/new', requiresAuth(), userController.postNew);
 
-router.get('/asignarRol',  userController.getRol);
+router.get('/asignarRol',  requiresAuth(), userController.getRol);
 
-router.post('/asignarRol', userController.postRol);
+router.post('/asignarRol', requiresAuth(), userController.postRol);
 
-router.get('/login', userController.getLogin);
-
-router.post('/login', userController.postLogin);
-
-router.get('/logout', userController.logout);
-
-router.get('/main', userController.getMain);
+router.get('/main',requiresAuth(), userController.getMain);
 
 module.exports = router;
