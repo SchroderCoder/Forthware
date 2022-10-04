c9256c6b41d3dfd9b5519ec515831f428a0fec98
DonCake <71788652+DonCake@users.noreply.github.com>
2022-10-03 19:47:59 -0500

    Se añade la funcion visualizar colaoradores con css

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'natdev',
+    database: 'natdev2',
     password: 'root',
     port:8889,
 });
