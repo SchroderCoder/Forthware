03d23ba85ce7d6091e9e3582c97488fb85ff09bb
unknown <a01707503@itesm.mx>
2022-10-06 21:41:41 -0500

    Crear reporte pt1

--- controllers/reportes.controller.js
+++ controllers/reportes.controller.js
@@ -35,12 +35,10 @@ exports.getCrearReporte = (request, response, next) => {
 };
 
 exports.postCrearReporte = (request, response, next) => {
-    console.log(request.body.proyectos);
     console.log(request.body.efectividad);
-    console.log(request.body.horas_base);
-    console.log(request.body.horas_ausencia);
+    console.log(request.body.horas);
+    console.log(request.body.nombres);
 
-    id_empleado= request.session.id_empleado;
     const reporte = new Reporte(request.body.fecha_inicio, request.body.fecha_fin, request.body.efectividad, 350, request.body.horas_base, request.body.horas_ausencia, .76, 2);
     reporte.save()
         .then(() => {
