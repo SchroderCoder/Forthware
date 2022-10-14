e6fb6e704a1f62aef8f713c097bf695473a1b5ac
A01703331 <a01703331@tec.mx>
2022-10-13 21:37:35 -0500

    Cambio en controller proyectos

--- controllers/proyectos.controller.js
+++ controllers/proyectos.controller.js
@@ -289,7 +289,7 @@ exports.getEditarEtiqueta = (request, response, next) => {
     Proyecto.fetchOne(request.params.id)
     .then(([rows, fielData]) => { 
         if (rows.length > 0) {
-            response.render(path.join('..',"views", "CrearEtiqueta.ejs"), {
+            response.render(path.join('..',"views", "crearEtiqueta.ejs"), {
                 proyectos: rows[0],
                 titulo: "Editar etiqueta " + rows[0].nombre,
                 etiquetas :rows[0],
