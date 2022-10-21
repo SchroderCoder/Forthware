051c670c18e37578b43af625895239cee27e49ce
A01703331 <a01703331@tec.mx>
2022-10-20 23:00:37 -0500

    Gestor de Proyectos NatDev - Version 1.0

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: '35.232.62.89',
     user: 'root',
     database: 'NatDev',
-    password: 'amogus',
+    password: 'Ce0f9LX8TYQpiScm7mNoQ4Hh5qkrn1uy6srUJtLyihJzn6L7QqDSAIlD1v6M',
     port: 3306,
 });
 
